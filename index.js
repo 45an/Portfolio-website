1a1a926834d32e8abd619f3d77c8adaf511d5b0e
45an <muse.yusuf@hotmail.com>
2022-11-13 23:05:30 +0100

    Minor Fix

--- index.js
+++ index.js
@@ -3,11 +3,9 @@ const navLinks = document.querySelector(".nav__link");
 
 navToggle.addEventListener("click", () => {
   document.body.classList.toggle("nav-open");
-  
 });
 
-
-let url = './resume.json';
+let url = "./resume.json";
 
 async function myResume() {
   let response = await fetch(url);
@@ -16,7 +14,7 @@ async function myResume() {
     let data = await response.json();
     console.log(data);
 
-    // container 
+    // container
     const jobExperiance = document.createElement("div");
     const education = document.createElement("div");
     jobExperiance.classList = "work";
@@ -36,7 +34,7 @@ async function myResume() {
 
     // text
 
-    const jobNote = document.createElement("p")
+    const jobNote = document.createElement("p");
     const educationNote = document.createElement("p");
     jobNote.classList = "job-note";
     educationNote.classList = "education-note";
@@ -44,48 +42,39 @@ async function myResume() {
     document.querySelector(".study").append(educationNote);
 
     //import text from JSON
-    
-    
 
-          for (let i = 0; i < data.jobexperience.length; i++) {
-            const workRole = document.createElement("i");
-            const workTitel = document.createElement("i");
-            const workDesc = document.createElement("i");
-            workRole.classList = "workRole";
-            workTitel.classList = "workTitel";
-            workDesc.classList = "workDesc";
-            document.querySelector(".job-note").append(workRole);
-            document.querySelector(".job-note").append(workTitel);
-            document.querySelector(".job-note").append(workDesc);
-            workRole.innerHTML = data.jobexperience[i].titel + "<br><br>";
-            workTitel.innerHTML = data.jobexperience[i].enterprise +"<br><br>";
-            workDesc.innerHTML = data.jobexperience[i].desc + "";
-          
-          }
-    
-    
+    for (let i = 0; i < data.jobexperience.length; i++) {
+      const workRole = document.createElement("i");
+      const workTitel = document.createElement("i");
+      const workDesc = document.createElement("i");
+      workRole.classList = "workRole";
+      workTitel.classList = "workTitel";
+      workDesc.classList = "workDesc";
+      document.querySelector(".job-note").append(workRole);
+      document.querySelector(".job-note").append(workTitel);
+      document.querySelector(".job-note").append(workDesc);
+      workRole.innerHTML = data.jobexperience[i].titel + "<br><br>";
+      workTitel.innerHTML = data.jobexperience[i].enterprise + "<br><br>";
+      workDesc.innerHTML = data.jobexperience[i].desc + "";
+    }
 
-        for (let i = 0; i < data.education.length; i++) {
-          const educationalRole = document.createElement("i");
-          const educationTitel = document.createElement("i");
-          const educationDesc = document.createElement("i");
-          educationalRole.classList = "educationalRole";
-          educationTitel.classList = "educationTitel";
-          educationDesc.classList = "educationDesc";
-          document.querySelector(".education-note").append(educationalRole);
-          document.querySelector(".education-note").append(educationTitel);
-          document.querySelector(".education-note").append(educationDesc);
-          educationalRole.innerHTML = data.education[i].titel + "<br>";
-          educationTitel.innerHTML = data.education[i].enterprise + "<br><br>";
-          educationDesc.innerHTML = data.education[i].desc + "<br><br>";
-        }
-
+    for (let i = 0; i < data.education.length; i++) {
+      const educationalRole = document.createElement("i");
+      const educationTitel = document.createElement("i");
+      const educationDesc = document.createElement("i");
+      educationalRole.classList = "educationalRole";
+      educationTitel.classList = "educationTitel";
+      educationDesc.classList = "educationDesc";
+      document.querySelector(".education-note").append(educationalRole);
+      document.querySelector(".education-note").append(educationTitel);
+      document.querySelector(".education-note").append(educationDesc);
+      educationalRole.innerHTML = data.education[i].titel + "<br>";
+      educationTitel.innerHTML = data.education[i].enterprise + "<br><br>";
+      educationDesc.innerHTML = data.education[i].desc + "<br><br>";
+    }
   } else {
     console.log("HTTP-Error: " + response.status);
   }
 }
 
-
 myResume();
-
-
